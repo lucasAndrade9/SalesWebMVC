3b04944f9bfa1ecb2214a5efc440c65868b473ff
Lucas Andrade <lucas.and227@gmai.com>
2020-05-21 16:53:51 -0400

    Seeding service

---
+++ SalesWebMVC/Data/SeedingService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SalesWebMVC.Models;
+using SalesWebMVC.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMVCContext _context;
+
+        public SeedingService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() || _context.Seller.Any() ||  _context.SalesRecord.Any())
+            {
+                return; //DB has been seeded
+            }
+
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Electronics");
+            Department d3 = new Department(3, "Fashion");
+            Department d4 = new Department(4, "Books");
+
+            Seller s1 = new Seller(1, "Bob brown", "bob@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+
+            SalesRecord r1 = new SalesRecord(1, new DateTime(2018, 09, 25), 11000.0, SaleStatus.Billed, s1);
+
+            _context.Department.AddRange(d1, d2, d3, d4);
+
+            _context.Seller.AddRange(s1);
+
+            _context.SalesRecord.AddRange(r1);
+
+            _context.SaveChanges();
+        }
+    }
+}
